8e1167a02582b8e182672db399cef3c99d997aa5
vignesh-wikki <vignesha445@gmail.com>
2024-12-26 14:27:24 +0530

    first commit

---
+++ Day1/HelloWorld.java
@@ -0,0 +1,10 @@
+
+// Create a simple Java program called HelloJava.java that prints "Hello, Java!".
+// Compile and run it from the command line using javac and java.
+
+class HelloWorld {
+    public static void main(String[] args)
+     {
+        System.out.print("Hello, Java!");
+    }
+}
---
+++ Day1/Ternary.java
@@ -0,0 +1,16 @@
+// Write a program that reads an integer and prints "Even" if it is even, or "Odd" if it is odd, using a ternary operator.
+
+import java.util.Scanner;
+
+
+public class Ternary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = sc.nextInt();
+
+        System.out.println(number % 2 ==0 ? "Even" : " Odd");
+
+        sc.close();
+    }
+}
